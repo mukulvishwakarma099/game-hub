991db626380d8200c49c910c964ec20428603c38
mukulvishwakarma099 <mukulvishwakarma099@gmail.com>
2023-12-05 11:51:03 +0530

    Add emoji to game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface GameCardProps {
   game: Game;
@@ -20,6 +21,7 @@ const GameCard = ({ game }: GameCardProps) => {
           <CriticScore key={game.id} score={game.metacritic} />
         </HStack>
         <Heading fontSize="2xl">{game?.name}</Heading>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
   );
