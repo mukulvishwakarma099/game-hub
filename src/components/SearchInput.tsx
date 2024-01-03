0e458097e33f208f3d248623a725d500d7e69ac0
mukulvishwakarma099 <mukulvishwakarma099@gmail.com>
2024-01-03 14:53:56 +0530

    Fix navbar and game deatils layout

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,17 +2,21 @@ import { useRef } from "react";
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store/store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
 
   const setSearchText = useGameQueryStore((state) => state.setSearchText);
 
+  const navigate = useNavigate();
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate("/");
       }}
     >
       <InputGroup>
